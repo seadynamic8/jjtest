e97121cecead25b4bd6f314710fd2b2005611449
seadynamic8 <seadynamic8@zoho.com>
2025-05-09 11:25:38 -0400

    it's important to comment our code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+// A "Hello world" program.
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <p>hello, world</p>
       <p>goodbye, world</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
