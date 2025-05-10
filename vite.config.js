0f9409dea78c99dba7c6c8860f5294768e6e816b
seadynamic8 <seadynamic8@zoho.com>
2025-05-10 11:34:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// add new change
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
